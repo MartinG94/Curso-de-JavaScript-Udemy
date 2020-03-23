4f7ce76ee541e6acca080a4eb0beaffac6e192ba
MartinG94 <guillenmartin94@gmail.com>
2020-03-23 02:07:21 -0300

    Utilizando Template Literals (nueva forma)

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 const productoA = 'Pizza',
-      precioA = 250,
+      precioA = 270,
       productoB = 'Hamburguesa',
-      precioB = 320;
+      precioB = 350;
 
 // Forma vieja de crear un Template de HTML
-let html = '<ul>' +
-            '<li>Orden: ' + productoA + '</li>' +
-            '<li>Precio: ' + precioA + '</li>' +
-            '<li>Orden: ' + productoB + '</li>' +
-            '<li>Precio: ' + precioB + '</li>' +
-            '<li>Total: ' + (precioA + precioB) + '</li>' +
-            '</ul>';
+// let html = '<ul>' +
+//             '<li>Orden: ' + productoA + '</li>' +
+//             '<li>Precio: ' + precioA + '</li>' +
+//             '<li>Orden: ' + productoB + '</li>' +
+//             '<li>Precio: ' + precioB + '</li>' +
+//             '<li>Total: ' + (precioA + precioB) + '</li>' +
+//             '</ul>';
+
+function calcularTotal(unPrecio, otroPrecio){
+    return unPrecio + otroPrecio;
+};
+
+html = `
+    <ul>
+        <li>Orden: ${productoA} </li>
+        <li>Precio: ${precioA} </li>
+        <li>Orden: ${productoB} </li>
+        <li>Precio: ${precioB} </li>
+        <li>Total: ${calcularTotal(precioA, precioB)} </li>
+    </ul>
+        `;
+
+// Las funciones pueden usarse dentro de los Templates Literals
 
 document.getElementById('app').innerHTML = html;
