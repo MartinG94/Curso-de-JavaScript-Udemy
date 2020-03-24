691dad8e5aa38ae1e95afd324758a2df78cb0a99
MartinG94 <guillenmartin94@gmail.com>
2020-03-24 01:05:26 -0300

    IIFE

--- app.js
+++ app.js
@@ -1,22 +1,6 @@
-// En JS Existe un objeto llamado Date
+//Creando un IIFE
 
-const diaHoy = new Date();
-
-let valor;
-//Mes: Enero = 0;
-valor = diaHoy.getMonth();
-//Dia
-valor = diaHoy.getDate();
-//Año
-valor = diaHoy.getFullYear();
-//Minutos
-valor = diaHoy.getMinutes();
-//Hora
-valor = diaHoy.getHours();
-//Milisegundos desde 1970
-valor = diaHoy.getTime();
-//Cambiar Año
-valor = diaHoy.setFullYear(2050);
-valor = diaHoy.getFullYear();
-
-console.log(valor);
+(function(nombre) {
+    console.log(`Hola Sr ${nombre}`);
+}
+)('Martín');
